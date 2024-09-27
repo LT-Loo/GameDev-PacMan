c5c689c07b27eaa42f8666ae7aee6832e0915e39
LT Loo <lertheng@gmail.com>
2024-09-27 17:28:13 +1000

    Added PacPumpkin to manual layout

--- Assets/Scripts/PacManager.cs
+++ Assets/Scripts/PacManager.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public class PacManager : MonoBehaviour
 {
     private Animator anim;
+    public bool isDead;
+
+    public Transform Target;
+    public Vector3 StartPos;
+    public Vector3 EndPos;
+    public float StartTime;
+    public float Duration;
+
     
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
-        anim.SetBool("isDead", true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        anim.SetBool("isDead", isDead);
     }
 }
